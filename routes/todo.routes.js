d9a9c89acd6cda217a9c23778f82858fac334919
findankit <122311036+findankit@users.noreply.github.com>
2023-04-30 13:25:26 +0530

    todo crud operations

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -10,7 +10,25 @@ const todoRouter = express.Router();
 //4.  Delete todo : delete : /todo/:id
 //5.  Update todo : patch : /todo/:id
 
-let todoArray = [];
+// apis:: https://www.postman.com/meetmajevadiya/workspace/nodejs-learning/collection/21824627-a798183a-9145-4429-b7a9-8cfbee6bb05d?action=share&creator=21824627
+
+let todoArray = [
+  {
+    todo_id: 1,
+    name: "Test",
+    description: "Something",
+  },
+  {
+    todo_id: 2,
+    name: "Test",
+    description: "Something",
+  },
+  {
+    todo_id: 3,
+    name: "Test",
+    description: "Something",
+  },
+];
 
 todoRouter.post("/todo", function (req, res) {
   let todoData = req.body;
@@ -48,6 +66,31 @@ todoRouter.get("/todo/:id", function (req, res) {
   });
 });
 
+todoRouter.delete("/todo/:id", function (req, res) {
+  let deleteId = req.params.id;
 
+  todoArray = todoArray.filter(function (el) {
+    return el.todo_id != deleteId;
+  });
+
+  res.status(200).json({
+    message: "Todo deleted successfully.",
+    todos: todoArray,
+  });
+});
+
+todoRouter.patch("/todo", function (req, res) {
+  let newData = req.body;
+
+  let todoIndex = todoArray.findIndex(function (todo) {
+    return todo.todo_id == newData.todo_id;
+  });
+
+  todoArray[todoIndex] = newData;
+
+  res.json({
+    message: "Success.",
+  });
+});
 
 module.exports = todoRouter;
