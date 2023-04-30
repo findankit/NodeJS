60c68e4e9cd215727b4f64867178ebc2053c11e4
findankit <122311036+findankit@users.noreply.github.com>
2023-04-30 13:33:01 +0530

    Add files via upload

--- javascript/array.js
+++ javascript/array.js
@@ -1,4 +1,4 @@
-let arr = [
+let todoArray = [
   {
     id: 12,
     name: "abc",
@@ -17,65 +17,92 @@ let arr = [
   },
 ];
 
+let newData = {
+  "todo_id": 33,
+  "name": "Test",
+  "description": "Something"
+};
+console.log(todoArray);
+// find index of id:33
+let index = todoArray.findIndex(function(todo) {
+  // console.log(todo);
+  return todo.id == 45;
+});
+console.log(index);
+// update value of that index
+todoArray[index] = newData;
+console.log(todoArray);
 
 
-// arr.unshift({ id: 3, name: "3rd todo" });
-// console.log(arr);
-// arr.shift();
-// console.log(arr);
-let deleteIndex = null;
-for (let i = 0; i < arr.length; i += 1) {
-  //   console.log(arr[i]);
-  let todo = arr[i];
-  if (todo.id === 33) {
-    deleteIndex = i;
-  }
-}
+
+// // arr.unshift({ id: 3, name: "3rd todo" });
+// // console.log(arr);
+// // arr.shift();
+// // console.log(arr);
+// let deleteIndex = null;
+// for (let i = 0; i < arr.length; i += 1) {
+//   //   console.log(arr[i]);
+//   let todo = arr[i];
+//   if (todo.id === 33) {
+//     deleteIndex = i;
+//   }
+// }
 
 // console.log(deleteIndex);
 
-let deletedItems = arr.splice(deleteIndex, 2);
+// // FINDINDEX
+// let findindex = null;
+// findindex = arr.findIndex(function (x) {
+//   return x.id == 33;
+// });
+// console.log(`-->`, findindex);
 
-// console.log(arr);
-// console.log(deletedItems);
+// // let deletedItems = arr.splice(deleteIndex, 1);
 
-arr[2] = {
+// // Using filter instead of arr.splice
+// let filteredArray = arr.filter(function (x) {
+//   return x.id != 33;
+// });
 
-}
-
-// let arr2 = new Array(3, "hi");
-
-// console.log(arr2);
+// console.log(filteredArray);
 
 
+// // console.log(arr);
+// // console.log(deletedItems);
 
-// Do these todos with array:
+// arr[2] = {};
 
-/*
-    1. For Loop over array
-    2. properties:
-       length
+// // let arr2 = new Array(3, "hi");
+
+// // console.log(arr2);
+
+// // Do these todos with array:
+
+// /*
+//     1. For Loop over array
+//     2. properties:
+//        length
     
-    3. methods:
-        push
-        pop
-        shift
-        unshift
-        splice
-*/
+//     3. methods:
+//         push
+//         pop
+//         shift
+//         unshift
+//         splice
+// */
 
-// -------------------
+// // -------------------
 
-/**
- * Other methods:
- * concat()
- * find()
- * findIndex()
- * reverse()
- * map()
- * filter()
- * sort()
- * forEach()
- * some()
- * every()
- */
+// /**
+//  * Other methods:
+//  * concat()
+//  * find()
+//  * findIndex()
+//  * reverse()
+//  * map()
+//  * filter()
+//  * sort()
+//  * forEach()
+//  * some()
+//  * every()
+//  */
